1955ecfc43abf062c8f51735a652f26f629d7ed6
Tim Otten <totten@civicrm.org>
2025-09-10 18:55:01 -0700

    Add Files.php

---
+++ src/Files.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Civi\SmartyClassifier;
+
+class Files {
+
+  public static function mkdir(string $dir): void {
+    if (!file_exists($dir)) {
+      mkdir($dir, 0777, TRUE);
+    }
+  }
+
+  public static function remove(string $glob): int {
+    $count = 0;
+    $files = glob($glob);
+    foreach ($files as $file) {
+      unlink($file);
+      $count++;
+    }
+    return $count;
+  }
+
+}
